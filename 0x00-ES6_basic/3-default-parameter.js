db3bdbbc955440993bce51aba5317644d7075a6d
zaragood <azaragoodness52@gmail.com>
2024-03-06 18:05:08 +0000

    defualt parameters

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989=81, expansion2019=19) {
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 81, expansion2019 =19 ) {
   return initialNumber + expansion1989 + expansion2019;
 }
