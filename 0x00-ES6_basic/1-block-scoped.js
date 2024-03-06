d8f62a9fb3e5dae33672b75d6b6b667e3f951392
zaragood <azaragoodness52@gmail.com>
2024-03-06 17:25:19 +0000

    arrow func and let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,7 +6,7 @@ export default function taskBlock(trueOrFalse) {
     const task = true;
     const task2 = false;
     if (task && task2) {
-        return [task, task2];
+      return [task, task2];
     }
   }
 
