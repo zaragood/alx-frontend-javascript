8a0a658ed67f4ed6aa18eb154254db088998e809
zaragood <azaragoodness52@gmail.com>
2024-03-08 18:36:35 +0000

    then and catch

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => ({ status: 200, body: 'success' }))
+  const body = { status: 200, body: 'success' };
+  return promise
+    .then(() => body)
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
