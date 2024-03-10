d30c5cb2f1af4799e9c47d90ac8fc12120ea96af
zaragood <azaragoodness52@gmail.com>
2024-03-10 21:15:55 +0000

    currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,13 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = code;
-    this._name = name;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = code;
+      this._name = name;
+    }
   }
 
   get code() {
---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw TypeError('amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (typeof currency === 'object' && currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw TypeError('currency must be of type currency');
+    }
+  }
+
+  displayFullPrice() {
+    return ` ${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
