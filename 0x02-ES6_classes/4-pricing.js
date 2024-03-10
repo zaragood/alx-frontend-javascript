ef4a2d5956c5cbbb9f3adb328eed32fcd9195c9f
zaragood <azaragoodness52@gmail.com>
2024-03-10 21:51:15 +0000

    checking 2

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,8 +2,12 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = amount;
+      this._currency = currency;
+    }
   }
 
   get amount() {
